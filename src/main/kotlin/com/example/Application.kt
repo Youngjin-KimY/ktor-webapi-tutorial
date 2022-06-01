515d408f6d8ccd30c1e3d834361ac4380d577dce
TimYj kim <timyjkim@TimYjui-MacBookPro.local>
2022-06-02 00:43:10 +0900

    first commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,14 @@
+package com.example
+
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+}
